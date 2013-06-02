bb571196f66214dd5ed72e265439ae9adc05e4c0
Miha Stajdohar <miha.stajdohar@gmail.com>
2013-06-03 01:36:35 +0200

    Added category icon and color.

---
+++ _network/widgets/__init__.py
@@ -0,0 +1,18 @@
+"""
+=======
+Network
+=======
+
+Network visualization and analysis tools for Orange.
+
+"""
+
+# Category description for the widget registry
+
+NAME = "Network"
+
+DESCRIPTION = "Network visualization and analysis tools for Orange."
+
+BACKGROUND = "#C0FF97"
+
+ICON = "icons/Category-Network.png"
