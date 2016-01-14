752c27dd608cb45cdd5731403123425375256737
Kernc <kerncece@gmail.com>
2016-01-14 19:44:32 +0100

    setup.py version bump to 1.1.1

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ from numpy.distutils.core import setup
 NAME = 'Orange3-Network'
 DOCUMENTATION_NAME = 'Orange Network'
 
-VERSION = '1.1.0'
+VERSION = '1.1.1'
 
 DESCRIPTION = 'Networks add-on for Orange 3 data mining software package.'
 LONG_DESCRIPTION  = open(os.path.join(os.path.dirname(__file__), 'README.rst')).read()
