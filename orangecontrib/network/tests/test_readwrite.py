1eccc2952de001adae39fda3e4d4095dc18edff6
janezd <janez.demsar@fri.uni-lj.si>
2019-04-19 16:05:15 +0200

    Add tests for readwrite and graphview

---
+++ orangecontrib/network/tests/test_readwrite.py
@@ -0,0 +1,21 @@
+from orangecontrib.network.network import readwrite
+
+import unittest
+
+class TestReadPajek(unittest.TestCase):
+    def test_two_mode(self):
+        davis = readwrite.read_pajek("../networks/davis.net")
+        self.assertEqual(davis.number_of_nodes(), 32)
+        self.assertEqual(
+            list(davis.nodes),
+            ['EVELYN', 'LAURA', 'THERESA', 'BRENDA', 'CHARLOTTE', 'FRANCES',
+             'ELEANOR', 'PEARL', 'RUTH', 'VERNE', 'MYRNA', 'KATHERINE',
+             'SYLVIA', 'NORA', 'HELEN', 'DOROTHY', 'OLIVIA', 'FLORA', 'E1',
+             'E2', 'E3', 'E4', 'E5', 'E6', 'E7', 'E8', 'E9', 'E10', 'E11',
+             'E12', 'E13', 'E14']
+            )
+        self.assertEqual(davis.in_first_mode, 18)
+
+
+if __name__ == "__main__":
+    unittest.main()
