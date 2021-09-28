3d9095164209aedc412dbb2f69bcd7483b518107
Primož Godec <p.godec9@gmail.com>
2021-09-28 15:39:38 +0200

    Remove markdown source parser

--- doc/conf.py
+++ doc/conf.py
@@ -33,8 +33,6 @@ sys.path.insert(0, os.path.abspath('..'))
 # If your documentation needs a minimal Sphinx version, state it here.
 #needs_sphinx = '1.0'
 
-source_parsers = {'.md': CommonMarkParser}
-
 # Add any Sphinx extension module names here, as strings. They can be
 # extensions coming with Sphinx (named 'sphinx.ext.*') or your custom
 # ones.
