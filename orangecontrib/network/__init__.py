11ef9065a4b795e29c9c725cfdac66b6dd5a8571
janezd <janez.demsar@fri.uni-lj.si>
2025-08-22 21:07:40 +0200

    Nx File: Fix Browse documentation datasets

--- orangecontrib/network/__init__.py
+++ orangecontrib/network/__init__.py
@@ -3,7 +3,7 @@ from importlib.resources import files, as_file
 def networks():
     path_in_package = files(__package__).joinpath('networks')
     with as_file(path_in_package) as dir_path:
-        yield ('', dir_path)
+        yield ('', str(dir_path))
 
 from .network import *
 
